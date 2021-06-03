782c59f3f9a885eb4f23152e03621fff84b7d0a3
unknown <lorp.lop@yandex.ru>
2021-06-03 14:13:07 +0300

    blabla

---
+++ WSUniveralLib/WSUniveralLib/Calculation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniveralLib
+{
+    public class Calculation
+    {
+        static double[] arrProduct = { 1.1f, 2.5f, 8.43f };
+        static double[] arrMaterial = { 1.003f, 1.0012f };
+        public int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            if ((productType > 3 || productType <= 0) || (materialType > 2 || productType <= 0))
+                return -1;
+            if (count <= 0 || width <= 0 || length <= 0)
+                return -1;
+
+            return Convert.ToInt32(
+               Math.Ceiling(arrProduct[productType - 1] * arrMaterial[materialType - 1] * count * width * length));
+        }
+    }
+}
